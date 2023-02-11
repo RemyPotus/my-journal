eababfdbd80a0f5ef5cd687d4b3fbe7b71263905
Rémy Potus <remy.potus@gmail.com>
2023-02-11 13:34:17 +0100

    feat: basic auth module

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,70 @@
-import { Controller, Get, Post, Param, Req, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Param,
+  Req,
+  Body,
+  Response,
+} from '@nestjs/common';
 import { UserService } from './user.service';
-import { Request, Response } from 'express';
+import { CreateUserDto, LoginDto } from './user.dto';
+import { User } from './user.entity';
 
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
 
-  @Get(':userUID')
-  async getJournalsByUserUID(@Param('userUID') userUID: string) {
-    const user = await this.userService.findOne(userUID);
-    return user;
+  /**
+   * Basic Auth
+   * @param userLoggingIn email+password
+   * @param res
+   * @returns
+   */
+  @Post('/login')
+  async login(@Body() userLoggingIn: LoginDto, @Response() res: any) {
+    if (!userLoggingIn.email || !userLoggingIn.password) {
+      return res.status(400).send({ message: 'Missing email or password.' });
+    }
+    const user: User | null = await this.userService.findByEmail(
+      userLoggingIn.email,
+    );
+    if (user === null) {
+      return res.status(404).json({ message: 'No matching email found.' });
+    } else {
+      if (user.password === userLoggingIn.password) {
+        const userInfo: Omit<User, 'password'> = user;
+        return res.status(200).json({ userInfo });
+      } else {
+        return res.status(401).json({ message: 'Invalid password or email.' });
+      }
+    }
+  }
+
+  /**
+   * Creates a new user if email not already in use
+   * @param createUser firstname + lastname + email + password
+   * @param res
+   * @returns
+   */
+  @Post('/register')
+  async register(@Body() createUser: CreateUserDto, @Response() res: any) {
+    const user: User | null = await this.userService.findByEmail(
+      createUser.email,
+    );
+    if (user === null) {
+      try {
+        const newUser: boolean = await this.userService.create(createUser);
+        if (newUser) {
+          return res
+            .status(200)
+            .json({ message: 'New user successfully created.' });
+        }
+      } catch (e) {
+        return res.status(500).json({ message: e });
+      }
+    } else {
+      return res.status(403).json({ message: 'Email already in use.' });
+    }
   }
 }
