220f88d514b0f9360e137f2f057d728db4704bc6
Rémy Potus <remy.potus@gmail.com>
2023-02-16 21:30:21 +0100

    feat: setup prisma connexion

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { PrismaClient } from '@prisma/client';
+
+export const prisma = new PrismaClient();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
