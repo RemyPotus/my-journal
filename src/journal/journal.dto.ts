ce352dbbcac2b02122804640b74da5254dbb8d80
Rémy Potus <remy.potus@gmail.com>
2023-02-25 17:13:18 +0100

    feat: swagger descriptions

--- src/journal/journal.dto.ts
+++ src/journal/journal.dto.ts
@@ -1,10 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateJournalDto {
+  @ApiProperty()
   userId: string;
+
+  @ApiProperty()
   name: string;
 }
 
 export class UpdateJournalDto {
+  @ApiProperty()
   id: string;
+
+  @ApiProperty()
   name?: string;
+
+  @ApiProperty()
   status?: string;
 }
--- src/user/user.dto.ts
+++ src/user/user.dto.ts
@@ -1,11 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty()
   firstName: string;
+
+  @ApiProperty()
   lastName: string;
+
+  @ApiProperty()
   email: string;
+
+  @ApiProperty()
   password: string;
 }
 
 export class LoginDto {
+  @ApiProperty()
   email: string;
+
+  @ApiProperty()
   password: string;
 }
